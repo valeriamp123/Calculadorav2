d11664d0653e3dbd50f96605117e051f2ebc6b3b
valeriamp123 <valeria.marulanda@correounivalle.edu.co>
2026-02-26 09:14:31 -0500

    Aplicacion con ruta para sumar

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template ("inicio.html")
+
+@app.route("/suma", methods=["GET","POST"])
+def sumar():
+    
+    if request.method == "POST":
+     if not request.form.get("numero 1") or not request.form.get("numero 2"):
+        return render_template("suma.html", error="Por favor ingrese ambos números")
+     else:
+      numero1=float(request.form.get("numero 1"))
+      numero2=float(request.form.get("numero 2"))
+      resultado=numero1+numero2
+      return render_template("suma.html", resultado=resultado)
+    return render_template("suma.html")
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
